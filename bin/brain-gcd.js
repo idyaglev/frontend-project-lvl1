5551f1596ea403ace4b64d1af08937a8727616e0
Ilya Dyaglev <dyaglev.work@gmail.com>
2020-04-20 15:37:00 +0300

    GCD game realization/Step 7

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import { gameStart } from '../src/index.js';
+import brainGcdGame from '../src/games/brainGcdGame.js';
+
+gameStart(brainGcdGame);
---
+++ src/games/brainGcdGame.js
@@ -0,0 +1,17 @@
+const gcd = (x, y) => {
+  if (y > x) return gcd(y, x);
+  if (!y) return x;
+  return gcd(y, x % y);
+};
+
+const brainGcdGame = () => {
+  const rule = 'Find the greatest common divisor of given numbers.';
+  const randomNum1 = Math.round(Math.random() * 100);
+  const randomNum2 = Math.round(Math.random() * 100);
+  const question = `${randomNum1} ${randomNum2}`;
+  const correctAnswer = gcd(randomNum1, randomNum2);
+  const result = [rule, question, correctAnswer];
+  return result;
+};
+
+export default brainGcdGame;
