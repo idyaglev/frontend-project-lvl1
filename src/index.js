3332b56ad8bf35ccc297944404a84dd3cbeb586d
Ilya Dyaglev <dyaglev.work@gmail.com>
2020-04-29 22:42:17 +0300

    Game Engine fix

--- src/index.js
+++ src/index.js
@@ -7,51 +7,29 @@ const greetingsFunction = () => {
   return userName;
 };
 
-const gameStart = (game = null) => {
-  let numberOfUsrWins = 0;
-  let result = '';
-  let incorrectAnswer = '';
-  let finalCorrectAnswer = '';
+const gameFlow = (rule, QandAGenerator) => {
   const userName = greetingsFunction();
-  if (game === null) {
-    return 'Please, choose your game ;)';
-  }
-  const rule = game()[0];
+  const winingMessage = `Congratulations, ${userName}!`;
+  let numberOfUserWins = 0;
+  let failureMessage = '';
   console.log(rule);
-  while (numberOfUsrWins < 3) {
-    const gameParameters = game();
-    const question = gameParameters[1];
-    const correctAnswer = gameParameters[2];
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    if (typeof correctAnswer === 'number') {
-      const intUserAnswer = userAnswer - 0;
-      if (intUserAnswer === correctAnswer) {
-        numberOfUsrWins += 1;
-        console.log('Correct!');
-      } else {
-        incorrectAnswer = userAnswer;
-        finalCorrectAnswer = correctAnswer;
-        break;
-      }
-    } else if (correctAnswer === userAnswer.toLowerCase() && typeof correctAnswer !== 'number') {
-      numberOfUsrWins += 1;
-      console.log('Correct!');
-    } else if (correctAnswer !== userAnswer.toLowerCase() && typeof correctAnswer !== 'number') {
-      incorrectAnswer = userAnswer;
-      finalCorrectAnswer = correctAnswer;
+  while (numberOfUserWins < 3) {
+    const questionAndAnswer = QandAGenerator();
+    console.log(`Question: ${questionAndAnswer[0]}`);
+    const userAnswer = readlineSync.question('Your answer ');
+    if (userAnswer !== questionAndAnswer[1]) {
+      failureMessage = `"${userAnswer}" is wrong answer ;(. Correct answer was "${questionAndAnswer[1]}"\nLet's try again, ${userName}!`;
       break;
+    } else {
+      console.log('Correct!');
+      numberOfUserWins += 1;
     }
   }
-
-  if (numberOfUsrWins === 3) {
-    result = `Congratulations, ${userName}!`;
-    console.log(result);
-    return result;
+  if (numberOfUserWins !== 3) {
+    console.log(failureMessage);
+  } else {
+    console.log(winingMessage);
   }
-  result = `${incorrectAnswer} is wrong answer ;(. Correct answer was ${finalCorrectAnswer}.\nLet's try again, ${userName}!`;
-  console.log(result);
-  return result;
 };
 
-export { greetingsFunction, gameStart };
+export default gameFlow;
